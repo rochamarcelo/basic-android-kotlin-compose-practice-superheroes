597bba29229d699247197052728fa32aeb4f8ba7
Marcelo Rocha <contato@omarcelo.com.br>
2025-05-06 17:52:21 -0300

    Add topbar

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -13,10 +13,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
@@ -59,15 +62,40 @@ fun HeroesListItem(hero: Hero, modifier: Modifier = Modifier) {
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HeroesTopBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge,
+                )
+            }
+        },
+        modifier = modifier
+    )
+
+}
 
 @Composable
 fun HeroesList (heroes: List<Hero>, modifier: Modifier = Modifier) {
     val paddingSmall = dimensionResource(R.dimen.padding_small);
     val paddingMedium = dimensionResource(R.dimen.padding_medium);
-    LazyColumn(modifier = modifier) {
-        items(heroes) { hero -> HeroesListItem(
-            hero = hero,
-            modifier = Modifier.padding(start = paddingMedium, end = paddingMedium, bottom = paddingSmall),
-        )}
+    Scaffold (
+        topBar = {
+            HeroesTopBar()
+        }
+    ) {
+        LazyColumn(contentPadding = it, modifier = modifier) {
+            items(heroes) { hero -> HeroesListItem(
+                hero = hero,
+                modifier = Modifier.padding(start = paddingMedium, end = paddingMedium, bottom = paddingSmall),
+            )}
+        }
     }
+
 }
